c9083755ea5196a7c3b7a9bc4245b1826e5bfb24
Muhammed003/bin/python3 <1samsungnik2019@gmail.com>
2022-06-14 10:57:47 +0600

    I add cors

--- apps/users/serializers.py
+++ apps/users/serializers.py
@@ -49,3 +49,45 @@ class RegisterSerializer(serializers.ModelSerializer):
 
     def create(self, validate_data):
         return CustomUser.objects.create_user(**validate_data)
+
+
+class ForgotPasswordSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+
+    def validate(self, attrs:dict):
+        email = attrs.get("email")
+        try:
+            CustomUser.objects.get(email=email)
+        except CustomUser.DoesNotExist:
+            raise serializers.ValidationError("Account with this email have not found")
+        return attrs
+
+
+class ResetPasswordSerializer(serializers.ModelSerializer):
+    activate_code = serializers.CharField(required=True)
+    password = serializers.CharField()
+    password_confirm = serializers.CharField()
+
+    class Meta:
+        model = CustomUser
+        fields = ('activate_code', 'password', 'password_confirm')
+
+    def validate(self, attrs):
+
+        password = attrs.get("password")
+        password_confirm = attrs.pop("password_confirm")
+
+        if len(password) < 8 or len(password_confirm) < 8:
+            raise serializers.ValidationError("Password is invalid")
+
+        if sum(c.isalpha() for c in password) < 1:
+            raise serializers.ValidationError("The password must contain at least 1 letter")
+
+        if sum(c.isdigit() for c in password) < 1:
+            raise serializers.ValidationError("The password must contain at least 1 number")
+
+
+        if password != password_confirm:
+            raise serializers.ValidationError("Password do not match")
+
+        return attrs
